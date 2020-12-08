4a49053845238628c15acc14d1bb372ab7bd5c54
Paulyne <paulynewambui2020@gmail.com>
2020-12-09 02:53:56 +0300

    added the login and logout redirect to home

--- movie/urls.py
+++ movie/urls.py
@@ -15,9 +15,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('post.urls')),
+       # Login and Logout
+    path('login/', auth_views.LoginView.as_view(redirect_authenticated_user=True,template_name='commons/login.html'
+        ), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='commons/logout.html'), name='logout'),
 ]
+
 
