7ed46d9ea126e39dbe0b4892806e4ed8350c7350
Paulyne <paulynewambui2020@gmail.com>
2020-12-10 07:11:54 +0300

    created api token and token refresh

--- movie/urls.py
+++ movie/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -24,6 +25,8 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(redirect_authenticated_user=True,template_name='commons/login.html'
         ), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='commons/logout.html'), name='logout'),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
 
 
