2a15647e1a8bed591fe2e0f003e655fb122155a8
Paulyne <paulynewambui2020@gmail.com>
2020-12-13 05:37:33 +0300

    added the cloudinary settings

--- movie/settings.py
+++ movie/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import cloudinary
+import cloudinary.uploader
+import cloudinary.api
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -145,3 +148,10 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 # typically, os.path.join(os.path.dirname(__file__), 'media')
 # MEDIA_ROOT = '<your_path>/media'
 # MEDIA_URL = '/media/'
+
+
+cloudinary.config( 
+ cloud_name = "dzv5d96u6", 
+ api_key =  "775292315189219",
+ api_secret = "q34G62AWtVzdXbM00uvWLZoU-Wc",
+)
