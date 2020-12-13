4346cff4d8c315ba8e0689525c4abb573cb779e4
Paulyne <paulynewambui2020@gmail.com>
2020-12-13 23:02:39 +0300

    added the simple jwt and its configuration

--- movie/settings.py
+++ movie/settings.py
@@ -14,6 +14,7 @@ import os
 import cloudinary
 import cloudinary.uploader
 import cloudinary.api
+from datetime import timedelta
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -131,13 +132,46 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 STATIC_URL = '/static/'
 
+
+
 REST_FRAMEWORK = {
+    
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+        ),
     'DEFAULT_AUTHENTICATION_CLASSES': [
         # 'rest_framework.authentication.BasicAuthentication',
         # 'rest_framework.authentication.SessionAuthentication',
-        'knox.auth.TokenAuthentication',
-        # 'rest_framework_simplejwt.authentication.JWTAuthentication',
+        # 'knox.auth.TokenAuthentication',
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
     ]
+}
+SIMPLE_JWT = {
+'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
+# 'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+# 'ROTATE_REFRESH_TOKENS': False,
+# 'BLACKLIST_AFTER_ROTATION': True,
+# 'UPDATE_LAST_LOGIN': False,
+
+# 'ALGORITHM': 'HS256',
+# 'SIGNING_KEY': settings.SECRET_KEY,
+# 'VERIFYING_KEY': None,
+# 'AUDIENCE': None,
+# 'ISSUER': None,
+
+# 'AUTH_HEADER_TYPES': ('Bearer',),
+# 'AUTH_HEADER_NAME': 'HTTP_AUTHORIZATION',
+'USER_ID_FIELD': 'id',
+# 'USER_ID_CLAIM': 'user_id',
+
+# 'AUTH_TOKEN_CLASSES': ('rest_framework_simplejwt.tokens.AccessToken',),
+# 'TOKEN_TYPE_CLAIM': 'token_type',
+
+# 'JTI_CLAIM': 'jti',
+
+# 'SLIDING_TOKEN_REFRESH_EXP_CLAIM': 'refresh_exp',
+# 'SLIDING_TOKEN_LIFETIME': timedelta(minutes=5),
+# 'SLIDING_TOKEN_REFRESH_LIFETIME': timedelta(days=1),
 }
 AUTH_USER_MODEL = 'post.User'
 LOGIN_REDIRECT_URL = 'home'
